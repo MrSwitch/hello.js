b2aa09de8d3d7a6e0093412e62c686e124eac043
Andrew Dodson <sonofdod@gmail.com>
2012-06-12 20:59:41 +1000

    Adding demos directory

---
+++ demos/client_ids.js
@@ -0,0 +1,17 @@
+	var local = ( /local/.test(window.location.host) ? 1 : 0);
+
+	// Var
+	var FACEBOOK_CLIENT_ID = [
+		'304672569582045',
+		'285836944766385'
+	][local];
+
+	// Var
+	var WINDOWS_CLIENT_ID = [
+		'0000000044088105',
+		'000000004405FD31'
+	][local];
+
+	// 
+	var GOOGLE_CLIENT_ID = '656984324806-sr0q9vq78tlna4hvhlmcgp2bs2ut8uj8.apps.googleusercontent.com';
+
