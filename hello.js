98df1a41c86223ef549ba810e86c16daca7535f0
Andrew Dodson <sonofdod@gmail.com>
2012-09-07 17:59:46 +1000

    Fixing a deprecated Google API... wish they'd said something

--- hello.js
+++ hello.js
@@ -58,14 +58,14 @@ var hello = (function(){
 			uri : {
 				// REF: http://code.google.com/apis/accounts/docs/OAuth2UserAgent.html
 				auth : "https://accounts.google.com/o/oauth2/auth",
-//				me	: "plus/v1/people/me?pp=1",
-				me : 'oauth2/v1/userinfo',
+				me	: "plus/v1/people/me?pp=1",
+//				me : 'oauth2/v1/userinfo',
 				base : "https://www.googleapis.com/",
 				'me/friends' : 'https://www.google.com/m8/feeds/contacts/default/full?alt=json&max-results=1000'
 
 			},
 			scope : {
-				basic : "https://www.googleapis.com/auth/plus.me,https://www.googleapis.com/auth/userinfo.profile,https://www.googleapis.com/auth/userinfo.email",
+				basic : "https://www.googleapis.com/auth/plus.me",
 				email			: '',
 				birthday		: '',
 				events			: '',
@@ -84,8 +84,9 @@ var hello = (function(){
 					if(o.id){
 						o.last_name = o.family_name || o.name.familyName;
 						o.first_name = o.given_name || o.name.givenName;
+//						o.name = o.first_name + ' ' + o.last_name;
 						o.picture = o.picture || o.image.url;
-						o.name = o.name || o.displayName;
+						o.name = o.displayName || o.name;
 					}
 					return o;
 				},
