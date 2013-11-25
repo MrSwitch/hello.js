3d506373510b1c2393a09948617830b5f11315d0
Andrew Dodson <sonofdod@gmail.com>
2013-11-25 22:56:51 +1100

    Test with phantomjs (by default), Report test code coverage karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,40 @@
 module.exports = function (karma) {
-    karma.set({
+	karma.set({
 
 // base path, that will be used to resolve files and exclude
-        basePath: './',
+		basePath: './',
 
-        frameworks: ['mocha', 'expect'],
+		frameworks: ['mocha', 'expect'],
 
 // list of files / patterns to load in the browser
-        files: [
+		files: [
 //            {pattern: '../node_modules/chai/chai.js', include: true},
-            { pattern: '../node_modules/sinon/pkg/sinon.js', watched: false, include: true },
-            '../src/*.js',
-            '../src/*/*.js',
-            '../tests/*.js'
-        ],
+			{ pattern: 'node_modules/sinon/pkg/sinon.js', watched: false, include: true },
+			'src/*.js',
+			'src/*/*.js',
+			'tests/*.js'
+		],
 
 
 // list of files to exclude
-        exclude: [
-            'karma.conf.js',
-            '../src/temp/*.js'
-        ],
+		exclude: [
+			'karma.conf.js',
+			'src/temp/*.js'
+		],
 
 
 // use dots reporter, as travis terminal does not support escaping sequences
 // possible values: 'dots', 'progress', 'junit', 'teamcity'
 // CLI --reporters progress
-        reporters: ['progress'],
+//		reporters: ['progress'],
 
-//        junitReporter: {
-            // will be resolved to basePath (in the same way as files/exclude patterns)
-//            outputFile: 'junit-report/test-results.xml'
- //       },
-/*
-        preprocessors: {
-            'src/*.js': 'coverage'
-        },
-*/
+
+		reporters: ['progress', 'coverage'],
+
+		preprocessors: {
+			'src/**/*.js': 'coverage'
+		},
+
 //Code Coverage options. report type available:
 //- html (default)
 //- lcov (lcov and html)
@@ -44,32 +42,37 @@ module.exports = function (karma) {
 //- text (standard output)
 //- text-summary (standard output)
 //- cobertura (xml format supported by Jenkins)
-     //   coverageReporter: {
-            // cf. http://gotwarlost.github.com/istanbul/public/apidocs/
-     // type: 'lcov',
-     //   dir: 'coverage/'
-     // },
+//		coverageReporter: {
+//			// cf. http://gotwarlost.github.com/istanbul/public/apidocs/
+//			type: 'lcov',
+//			dir: '../coverage/'
+//		},
+
+//        junitReporter: {
+			// will be resolved to basePath (in the same way as files/exclude patterns)
+//            outputFile: 'junit-report/test-results.xml'
+//       },
 
 
 // web server port
-        port: 9876,
+		port: 9876,
 
 
 // cli runner port
-        runnerPort: 9100,
+		runnerPort: 9100,
 
 
 // enable / disable colors in the output (reporters and logs)
-        colors: true,
+		colors: true,
 
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: karma.LOG_DEBUG,
+		logLevel: karma.LOG_DEBUG,
 
 
 // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+		autoWatch: true,
 
 
 // Start these browsers, currently available:
@@ -81,11 +84,11 @@ module.exports = function (karma) {
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-        browsers: ['Chrome'],
+		browsers: ['PhantomJS'],
 
 
 // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 6000,
+		captureTimeout: 6000,
 
 
 // Continuous Integration mode
@@ -93,15 +96,15 @@ module.exports = function (karma) {
 //        singleRun: true,
 
 
-        plugins: [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-expect',
-            'karma-mocha'
-            //,
+		plugins: [
+			'karma-phantomjs-launcher',
+			'karma-coverage',
+			'karma-expect',
+			'karma-mocha'
+			//,
 //            'karma-jasmine'
    //         'karma-junit-reporter',
    //         'karma-coverage'
-        ]
-    });
+		]
+	});
 }
