31f01dbf3c9400d6ffcb552c2136a0200a474187
Andrew Dodson <sonofdod@gmail.com>
2014-03-27 08:25:51 +1100

    Update friend demo to get me/contacts, fix windows me/contacts to show user_id images where available, #37

--- src/modules/windows.js
+++ src/modules/windows.js
@@ -10,7 +10,12 @@ function formatUser(o){
 		if(o.emails){
 			o.email =  o.emails.preferred;
 		}
-		o.thumbnail = o.picture = 'https://apis.live.net/v5.0/'+o.id+'/picture?access_token='+token;
+		// If this is not an non-network friend
+		if(o.is_friend!==false){
+			// Use the id of the user_id if available
+			var id = (o.user_id||o.id);
+			o.thumbnail = o.picture = 'https://apis.live.net/v5.0/'+id+'/picture?access_token='+token;
+		}
 	}
 }
 
