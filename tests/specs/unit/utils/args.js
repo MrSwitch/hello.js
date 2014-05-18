c47456b995f4a07598b965408287857237de39a9
Andrew Dodson <sonofdod@gmail.com>
2014-05-18 21:05:30 +1000

    Spec utils.args

---
+++ tests/specs/unit/utils/args.js
@@ -0,0 +1,73 @@
+define([
+//	'../../../../src/utils/diff'
+], function(
+//	diff
+){
+
+var args = hello.utils.args;
+
+//
+// Events
+//
+describe('utils / args', function(){
+
+
+	it('should accept an object and arguments as first and second parameters and return an object', function(){
+
+		var value = args({}, []);
+
+		expect( value ).to.be.an( Object );
+
+	});
+
+
+	it('should map arguments to an object', function(){
+
+		var value = args({str:'s', obj: 'o', func : 'f'}, ['String', {}, function(){}]);
+
+		expect( value ).to.be.an( 'object' );
+
+		expect( value.str ).to.be.a( 'string' );
+
+		expect( value.obj ).to.be.an( 'object' );
+
+		expect( value.func ).to.be.a( 'function' );
+
+	});
+
+
+
+	it('should interpret the order of arguments, so some can be ommited', function(){
+
+		var value = args({str:'s', obj: 'o', func : 'f'}, [function(){}]);
+
+		expect( value ).to.be.an( 'object' );
+
+		expect( value ).to.not.have.property('str');
+
+		expect( value ).to.not.have.property('obj');
+
+		expect( value.func ).to.be.a( 'function' );
+
+	});
+
+	it('should decipher whether the first argument is all the arguments represented as an object', function(){
+
+		var value = args({str:'s', obj: 'o', func : 'f'}, [{
+			func : function(){}
+		}]);
+
+		expect( value ).to.be.an( 'object' );
+
+		expect( value ).to.not.have.property('str');
+
+		expect( value ).to.not.have.property('obj');
+
+		expect( value.func ).to.be.a( 'function' );
+
+	});
+
+});
+
+	
+});
