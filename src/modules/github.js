274815c3f7caca2ed2817a4c47dd69386a0fe976
Andrew Dodson <sonofdod@gmail.com>
2014-10-04 19:26:55 +1000

    Github serialize payload to JSON, fix #116

--- src/modules/github.js
+++ src/modules/github.js
@@ -74,6 +74,19 @@ hello.init({
 				}
 				return o;
 			}
+		},
+		xhr : function(p){
+
+			if( p.method !== 'get' && p.data ){
+				// Serialize payload as JSON
+				p.headers = p.headers || {};
+				p.headers['Content-Type'] = 'application/json';
+				if (typeof(p.data) === 'object'){
+					p.data = JSON.stringify(p.data);
+				}
+			}
+
+			return true;
 		}
 	}
 });
