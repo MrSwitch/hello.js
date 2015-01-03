79015c57437c1e30edc047809c1596328eabede8
Will Bamford <w.bamford@gmail.com>
2015-01-03 18:43:07 +0000

    Add Facebook and Instagram tests for "/me" path.

---
+++ tests/specs/unit/core/modules.js
@@ -0,0 +1,73 @@
+define([], function () {
+
+	describe('modules', function () {
+
+    var request = hello.utils.request;
+
+    var requestProxy = function (req, callback) {
+
+      console.log(req);
+
+      var r = {
+        network: req.network,
+        method: req.method,
+        url: req.url,
+        data: req.data,
+        xhr: true
+      };
+
+      r.url = './stubs/' + req.network + '/' + req.method + '/' + req.path + '.json';
+
+      console.log(r.url);
+
+      request.call(hello.utils, r, callback);
+    };
+
+    before(function () {
+      hello.utils.request = requestProxy;
+    });
+
+    after(function () {
+      hello.utils.request = request;
+    });
+
+    describe.only('/me', function () {
+
+      var tests = [];
+
+      tests.push({
+        network: "facebook",
+        expect: {
+          id: "id-12345678",
+          name: "Full Name",
+          thumbnail: "http://graph.facebook.com/id-12345678/picture"
+        }
+      });
+
+      tests.push({
+        network: "instagram",
+        expect: {
+          id: "1012345678",
+          name: "My Name",
+          thumbnail: "https://www.examples.com/instagram-profile.jpg"
+        }
+      });
+
+      hello.utils.forEach(tests, function (test) {
+
+        it('should format ' + test.network + ' correctly', function (done) {
+          hello(test.network).api('/me', function (me) {
+            expect(me.id).to.be(test.expect.id);
+            expect(me.name).to.be(test.expect.name);
+            expect(me.thumbnail).to.be(test.expect.thumbnail);
+            done();
+          });
+        });
+
+      });
+
+    });
+
+	});
+
+});
