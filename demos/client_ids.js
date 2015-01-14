5150aac353642318a181559928a9cbed996026c4
Andrew Dodson <sonofdod@gmail.com>
2015-01-14 19:18:49 +0000

    Update client_ids for https://adodson.com

--- demos/client_ids.js
+++ demos/client_ids.js
@@ -3,6 +3,9 @@
 // Defines the CLIENT_ID (AppID's) of the OAuth2 providers
 // relative to the domain host where this code is presented.
 
+var location_https = window.location.href.indexOf('https://') === 0;
+
+
 // Register your domain with Facebook at  and add here
 var FACEBOOK_CLIENT_ID = {
 	'adodson.com' : '160981280706879',
@@ -17,7 +20,8 @@ var WINDOWS_CLIENT_ID = {
 	'local.knarly.com' : '000000004405FD31'
 }[window.location.hostname];
 
-//
+
+// Google Register --  https://console.developers.google.com
 var GOOGLE_CLIENT_ID = '656984324806-sr0q9vq78tlna4hvhlmcgp2bs2ut8uj8.apps.googleusercontent.com';
 
 // To make it a little easier
@@ -30,6 +34,7 @@ var CLIENT_IDS = {
 // Dropbox full 't5s644xtv7n4oth'... requires production authentication
 var DROPBOX_CLIENT_ID = '1lkagy1bz7h2uhl';
 
+// LinkedIn Register - https://www.linkedin.com/secure/developer
 var LINKEDIN_CLIENT_ID = 'bixrjszkfk0j'; // 'exgsps7wo5o7'
 
 var YAHOO_CLIENT_ID = {
@@ -42,24 +47,29 @@ var TWITTER_CLIENT_ID = {
 	'adodson.com' : 'eQuyZuECKWPiv3D7E4qdg'
 }[window.location.hostname];
 
+
+// SoundCloud Register - http://soundcloud.com/you/apps/
 var SOUNDCLOUD_CLIENT_ID = {
 	'local.knarly.com' : '8a4a19f86cdab097fa71a15ab26a01d6',
-	'adodson.com' : '47a386647dadf913e559c12ef6db4292'
+	'adodson.com' : location_https ? 'eacfb4d662e046c19e53c9e44bf7f7b7' : '47a386647dadf913e559c12ef6db4292'
 }[window.location.hostname];
 
+// FourSquare Register - https://foursquare.com/developers/apps
 var FOURSQUARE_CLIENT_ID = {
 	'local.knarly.com' : '3HEXMBQVH2SV0VXUKXOGQRPWH1PUTEIZN4KBDY5L54ZDXCDP',
 	'adodson.com' : 'MFRXCJP1TKMUSYC2JBYU50L0IH0GJP1HTNS1BV0ML3NNXG5B'
 }[window.location.hostname];
 
+// Github Register - https://github.com/settings/applications
 var GITHUB_CLIENT_ID = {
 	'local.knarly.com' : 'ca7e06a718b2e8eef737',
-	'adodson.com' : 'd934ef34e2e40cf9b00a'
+	'adodson.com' : location_https ? 'c379ed1ef504fccfa85c' : 'd934ef34e2e40cf9b00a'
 }[window.location.hostname];
 
+// Instagram Register - http://instagram.com/developer/clients/manage/
 var INSTAGRAM_CLIENT_ID = {
 	'local.knarly.com' : 'bfbbf362ac3148aeb1150e5b8256bbe9',
-	'adodson.com' : '264d13a33ba845f396a152cc326e6f5d'
+	'adodson.com' : location_https ? 'e47d210f864c4b1ca94225ddab97205a' : '264d13a33ba845f396a152cc326e6f5d'
 }[window.location.hostname];
 
 var BOX_CLIENT_ID = {
