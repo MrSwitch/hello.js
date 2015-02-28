87ba76fbd79c71ed9120aaa206c091fa8f11203d
Andrew Dodson <andrew.j.dodson@gmail.com>
2015-02-28 14:37:11 +1100

    Spec legacy events, subscribe

--- tests/specs/unit/core/hello.events.js
+++ tests/specs/unit/core/hello.events.js
@@ -4,19 +4,24 @@
 describe('hello events', function(){
 
 	it('should bind handler using hello.on( event_name, handler ) and trigger hello.emit', function(done){
-		hello.on('auth.login', function(){
+		function handler(){
 			done();
-		});
+		}
 
+		hello.on('auth.login', handler);
 		hello.emit('auth.login');
+		hello.off('auth.login', handler);
 	});
 
 	it('should support legacy handlers using hello.subscribe( event_name, handler ) and trigger hello.emit', function(done){
-		hello.subscribe('auth.login', function(){
-			done();
-		});
 
+		function handler(){
+			done();
+		}
+
+		hello.subscribe('auth.login', handler);
 		hello.emit('auth.login');
+		hello.unsubscribe('auth.login', handler);
 	});
 
 });
