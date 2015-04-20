e4055ed05f4b1ceac4f749530f6663c4b3ee42c7
Will Bamford <w.bamford@gmail.com>
2015-04-20 14:38:07 +0100

    Add Gruntfile.js to JSCS tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,73 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js'],
-      options: {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js'],
+			options: {
+				ignores: ['tests/specs/libs/*.js'],
+				globals: {
+					console: true,
+					module: true,
+					document: true
+				},
+				sub: true
+			}
+		},
+		jscs: {
+			src: ['Gruntfile.js', 'src/hello.js'],
+			options: {
+				config: '.jscsrc'
+			}
+		},
+		mocha_phantomjs: {
+			all: ['tests/specs/index.html']
+		},
+		bumpup: ['package.json', 'bower.json'],
+		shunt: {
+			docs: {
+				'README.md': './index.html'
+			},
+			build: {
+				'dist/hello.js': ['src/hello.js', 'src/hello.legacy.js', 'src/hello.amd.js', 'src/hello.commonjs.js'],
+				'dist/hello.all.js': [
+					'src/hello.js',
+					'src/hello.legacy.js',
+					'src/modules/dropbox.js',
+					'src/modules/facebook.js',
+					'src/modules/flickr.js',
+					'src/modules/foursquare.js',
+					'src/modules/github.js',
+					'src/modules/google.js',
+					'src/modules/instagram.js',
+					'src/modules/linkedin.js',
+					'src/modules/soundcloud.js',
+					'src/modules/twitter.js',
+					'src/modules/windows.js',
+					'src/modules/yahoo.js',
+					'src/hello.amd.js',
+					'src/hello.commonjs.js'
+				]
+			},
+			minify: {
+				'dist/hello.min.js': 'dist/hello.js',
+				'dist/hello.all.min.js': 'dist/hello.all.js'
+			}
+		},
+		watch: {
+			files: ['src/**/*.js'],
+			tasks: ['jscs']
+		}
+	});
 
-        ignores: ['tests/specs/libs/*.js'],
+	grunt.loadNpmTasks('grunt-bumpup');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-mocha-phantomjs');
+	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('shunt');
 
-        // Options here to override JSHint defaults
-        globals: {
-          console: true,
-          module: true,
-          document: true
-        },
-        // Don't check dot notation
-        sub: true
-      }
-    },
-    // Code formatting / linting
-    jscs: {
-      src: 'src/hello.js',
-      options: {
-          config: '.jscsrc'
-      }
-    },
-    // JS unit / E2E tests
-    mocha_phantomjs: {
-      all: ['tests/specs/index.html'],
-    },
-    // Bump
-    bumpup: ['package.json', 'bower.json'],
-    // Shunt files around
-    shunt: {
-      docs: {
-        'README.md': './index.html'
-      },
-      build: {
-        'dist/hello.js': ['src/hello.js', 'src/hello.legacy.js', 'src/hello.amd.js', 'src/hello.commonjs.js'],
-        'dist/hello.all.js': [
-          'src/hello.js',
-          'src/hello.legacy.js',
-          'src/modules/dropbox.js',
-          'src/modules/facebook.js',
-          'src/modules/flickr.js',
-          'src/modules/foursquare.js',
-          'src/modules/github.js',
-          'src/modules/google.js',
-          'src/modules/instagram.js',
-          'src/modules/linkedin.js',
-          'src/modules/soundcloud.js',
-          'src/modules/twitter.js',
-          'src/modules/windows.js',
-          'src/modules/yahoo.js',
-          'src/hello.amd.js',
-          'src/hello.commonjs.js'
-        ]
-      },
-      minify: {
-        'dist/hello.min.js': 'dist/hello.js',
-        'dist/hello.all.min.js': 'dist/hello.all.js'
-      }
-    },
-    watch: {
-      files: ['src/**/*.js'],
-      tasks: ['jscs']
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-bumpup');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-phantomjs');
-  grunt.loadNpmTasks("grunt-jscs");
-  grunt.loadNpmTasks('shunt');
-
-  grunt.registerTask('test', ['jscs', 'jshint', 'mocha_phantomjs']);
-  grunt.registerTask('default', ['test', 'shunt:build', 'shunt:minify']);
+	grunt.registerTask('test', ['jscs', 'jshint', 'mocha_phantomjs']);
+	grunt.registerTask('default', ['test', 'shunt:build', 'shunt:minify']);
 
 };
