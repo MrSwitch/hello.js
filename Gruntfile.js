38da636d2b5b8755b45bc917ab229e3a5ddea6b1
Will Bamford <w.bamford@gmail.com>
2015-04-21 08:38:35 +0100

    Use 'mocha' alias instead of 'mocha_phantomjs'

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('shunt');
 
 	grunt.registerTask('mocha', ['mocha_phantomjs']);
-	grunt.registerTask('test', ['jscs', 'jshint', 'mocha_phantomjs']);
+	grunt.registerTask('test', ['jscs', 'jshint', 'mocha']);
 	grunt.registerTask('default', ['test', 'shunt:build', 'shunt:minify']);
 
 };
