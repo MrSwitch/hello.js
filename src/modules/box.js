1d27f064522ecb44a86be92a4df7a5dbbf31fac0
Will Bamford <w.bamford@gmail.com>
2015-04-21 07:23:29 +0100

    JSCS box module

--- src/modules/box.js
+++ src/modules/box.js
@@ -1,46 +1,53 @@
-//
-// GitHub
-//
 hello.init({
-	box : {
-		name : 'Box',
-		oauth : {
-			version : 2,
-			auth : 'https://app.box.com/api/oauth2/authorize',
-			grant : 'https://api.box.com/oauth2/token',
-			response_type : 'code'
+	box: {
+		name: 'Box',
+
+		oauth: {
+			version: 2,
+			auth: 'https://app.box.com/api/oauth2/authorize',
+			grant: 'https://api.box.com/oauth2/token',
+			response_type: 'code'
 		},
-		base : 'https://api.box.com/2.0/',
-		get : {
-			'me' : 'users/me',
-			'me/files' : 'files'
+
+		base: 'https://api.box.com/2.0/',
+
+		get: {
+			me: 'users/me',
+			'me/files': 'files'
 		},
-		scope : {
-			basic : ''
+
+		scope: {
+			basic: ''
 		},
-		wrap : {
-			me : function(o){
-				if(o.id){
+
+		wrap: {
+			me: function(o) {
+				if (o.id) {
 					o.picture = o.thumbnail = o.avatar_url;
-					if(o.login.match('@')){
+					if (o.login.match('@')) {
 						o.email = o.login;
 					}
 				}
+
 				return o;
 			},
-			"me/files" : function(o){
-				if(Object.prototype.toString.call(o) === '[object Array]'){
+
+			'me/files': function(o) {
+				if (Object.prototype.toString.call(o) === '[object Array]') {
 					return {data:o};
 				}
+
 				return o;
 			}
 		},
-		xhr : function(p){
+
+		xhr: function(p) {
 
 			p.proxy = true;
 			p.proxy_response_type = 'proxy';
 			return true;
 		},
-		jsonp : false
+
+		jsonp: false
 	}
 });
