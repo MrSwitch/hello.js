811dbb7456fe2ed78aa4ab738a7696f6d52cca9f
Will Bamford <w.bamford@gmail.com>
2015-04-23 00:45:51 +0100

    Begin JSCS tests files

--- tests/specs/e2e/modules.js
+++ tests/specs/e2e/modules.js
@@ -15,71 +15,63 @@ define([
 	'../../../src/modules/bikeindex',
 	'../../../src/modules/soundcloud'
 ], function(
-//	hello
-	error_response
-){
+	errorResponse
+) {
 
-//
-// Modules are of the following formats
-//
+	describe('E2E modules', function() {
 
-describe('E2E Modules', function(){
+		// Loop through all services
+		for (var name in hello.services) {
+			setupModuleTests(hello.services[name], name);
+		}
 
-// Loop through all services
-for(var name in hello.services){
-	setup_module_tests(hello.services[name], name);
-}
+		function setupModuleTests(module, name) {
 
-function setup_module_tests(module, name){
+			describe(name, function() {
 
-	describe( name , function(){
+				var MATCH_URL = /^https?\:\/\//;
 
-		var MATCH_URL = /^https?\:\/\//;
-
-		it('contain oauth.auth path', function(){
-			var path = module.oauth.auth;
-			expect( path ).to.match( /^https?\:\/\// );
-		});
-
-		it('specify a base url', function(){
-			// Loop through all services
-			expect( module.base ).to.match( /^https?\:\/\// );
-		});
-
-
-		it('using OAuth1 contain, auth, request, token properties', function(){
-
-			// Loop through all services
-			var oauth = module.oauth;
-			if(oauth && parseInt(oauth.version,10) === 1 ){
-				expect( oauth.auth ).to.match( MATCH_URL );
-				expect( oauth.token ).to.match( MATCH_URL );
-				expect( oauth.request ).to.match( MATCH_URL );
-			}
-		});
-
-		xit('return error object when an api request is made with an unverified user', function(done){
-
-			var i=0;
-
-			this.timeout(60000);
-
-			var cb = error_response(null, function(){
-				if(++i===2)
-					done();
+				it('contain oauth.auth path', function() {
+				var path = module.oauth.auth;
+				expect(path).to.match(/^https?\:\/\//);
 			});
 
-			// ensure user is signed out
-			hello.logout(name);
+				it('specify a base url', function() {
+				// Loop through all services
+				expect(module.base).to.match(/^https?\:\/\//);
+			});
 
-			// Make a request that returns an error object
-			hello(name)
-			.api('me', cb)
-			.then(null, cb);
-		});
-		/**/
+				it('using OAuth1 contain, auth, request, token properties', function() {
 
+					// Loop through all services
+					var oauth = module.oauth;
+					if (oauth && parseInt(oauth.version, 10) === 1) {
+						expect(oauth.auth).to.match(MATCH_URL);
+						expect(oauth.token).to.match(MATCH_URL);
+						expect(oauth.request).to.match(MATCH_URL);
+					}
+				});
+
+				xit('return error object when an api request is made with an unverified user', function(done) {
+
+					var i = 0;
+
+					this.timeout(60000);
+
+					var cb = errorResponse(null, function() {
+					if (++i === 2)
+						done();
+				});
+
+					// ensure user is signed out
+					hello.logout(name);
+
+					// Make a request that returns an error object
+					hello(name)
+					.api('me', cb)
+					.then(null, cb);
+				});
+			});
+		}
 	});
-}
-});
 });
--- tests/specs/unit/core/hello.api.js
+++ tests/specs/unit/core/hello.api.js
@@ -1,52 +1,48 @@
 define([
-//	'../../../src/hello'
 	'../../libs/error_response'
 ], function(
-//	hello
-	error_response
-){
+	errorResponse
+) {
 
-
-//
-// Api
-//
-describe('hello.api', function(){
+describe('hello.api', function() {
 
 	var _request = hello.utils.request;
 	var _store = hello.utils.store;
 	var _session = {
-		access_token : 'token'
+		access_token: 'token'
 	};
 	var testable = {};
 
-	before(function(){
+	before(function() {
 
-		// unset
+		// Unset
 		_request = hello.utils.request;
 		_store = hello.utils.store;
 
 		// Mock request
-		hello.utils.request = function(req, callback){
-			setTimeout(function(){
+		hello.utils.request = function(req, callback) {
+			setTimeout(function() {
 				callback(req);
 			});
 		};
 
-		hello.utils.store = function(label, body){
+		hello.utils.store = function(label, body) {
 			return _session;
 		};
 
 		// Define default network
 		hello.init({
-			testable : {
-				oauth : {
-					auth : 'https://testdemo/access',
-					version : 2
+			testable: {
+				oauth: {
+					auth: 'https://testdemo/access',
+					version: 2
 				},
-				scope : {
-					'basic' : 'basic_scope'
+
+				scope: {
+					basic: 'basic_scope'
 				},
-				base : 'https://testable/'
+
+				base: 'https://testable/'
 			}
 		});
 
@@ -54,126 +50,123 @@ describe('hello.api', function(){
 
 	});
 
-	after(function(){
+	after(function() {
 
 		// renew
 		hello.utils.request = _request;
 
 	});
 
-	it('should assign a complete event', function(done){
+	it('should assign a complete event', function(done) {
 		hello('test')
-		.api('/', function(){done();});
+		.api('/', function() {done();});
 	});
 
-	it('should throw a completed event if network name is undefined', function(done){
+	it('should throw a completed event if network name is undefined', function(done) {
 		hello('test')
-		.api('/', error_response("invalid_network", done) );
+		.api('/', errorResponse('invalid_network', done));
 	});
 
-	it('should throw a error event if network name is undefined', function(done){
+	it('should throw a error event if network name is undefined', function(done) {
 		hello('test')
-		.api("/")
-		.then( null, error_response("invalid_network", done) );
+		.api('/')
+		.then(null, errorResponse('invalid_network', done));
 	});
 
-	it('should throw a error event if path name is undefined', function(done){
+	it('should throw a error event if path name is undefined', function(done) {
 		hello('test')
 		.api()
-		.then( null, error_response("invalid_path", done) );
+		.then(null, errorResponse('invalid_path', done));
 	});
 
-	it('should construct the url using the base and the pathname', function(done){
+	it('should construct the url using the base and the pathname', function(done) {
 
 		hello('testable')
-		.api("/endpoint", function(res){
-			expect( res.url ).to.eql( 'https://testable/endpoint' );
+		.api('/endpoint', function(res) {
+			expect(res.url).to.eql('https://testable/endpoint');
 			done();
 		});
 	});
 
-
-	it('should extract the parameters from the URL', function(done){
+	it('should extract the parameters from the URL', function(done) {
 
 		var session = _session;
 		_session = null;
 
 		hello('testable')
-		.api("/endpoint?a=a&b=b", function(res){
+		.api('/endpoint?a=a&b=b', function(res) {
 			_session = session;
-			expect( res.url ).to.eql( 'https://testable/endpoint' );
-			expect( res.query ).to.eql({
-				a : 'a',
-				b : 'b'
+			expect(res.url).to.eql('https://testable/endpoint');
+			expect(res.query).to.eql({
+				a: 'a',
+				b: 'b'
 			});
 			done();
 		});
 	});
 
-	it('should attach query object to the req.query', function(done){
+	it('should attach query object to the req.query', function(done) {
 
 		hello('testable')
-		.api("/endpoint", {a:'a'}, function(res){
+		.api('/endpoint', {a: 'a'}, function(res) {
 
-			expect( res.query ).to.have.property('a','a');
+			expect(res.query).to.have.property('a', 'a');
 
 			done();
 		});
 	});
 
-	it('should attach data object to the req.query when `req.method = get`', function(done){
+	it('should attach data object to the req.query when `req.method = get`', function(done) {
 
 		hello('testable')
-		.api("/endpoint", 'get', {a:'a'}, function(res){
+		.api('/endpoint', 'get', {a: 'a'}, function(res) {
 
-			expect( res.query ).to.have.property('a','a');
-			expect( res.data ).to.be.empty();
+			expect(res.query).to.have.property('a', 'a');
+			expect(res.data).to.be.empty();
 
 			done();
 		});
 	});
 
-
-	it('should attach post data object to the req.data', function(done){
+	it('should attach post data object to the req.data', function(done) {
 
 		hello('testable')
-		.api("/endpoint", 'post', {a:'a'}, function(res){
+		.api('/endpoint', 'post', {a: 'a'}, function(res) {
 
-			expect( res.method ).to.eql( 'post' );
-			expect( res.query ).to.not.have.property('a');
-			expect( res.data ).to.have.property('a','a');
+			expect(res.method).to.eql('post');
+			expect(res.query).to.not.have.property('a');
+			expect(res.data).to.have.property('a', 'a');
 
 			done();
 		});
 	});
 
-	describe("signing", function(){
+	describe('signing', function() {
 
-		it('should add the access_token to the req.query', function(done){
+		it('should add the access_token to the req.query', function(done) {
 
 			hello('testable')
-			.api("/endpoint", function(res){
-				expect( res.url ).to.eql( 'https://testable/endpoint' );
-				expect( res.query ).to.eql( _session );
+			.api('/endpoint', function(res) {
+				expect(res.url).to.eql('https://testable/endpoint');
+				expect(res.query).to.eql(_session);
 				done();
 			});
 		});
 
-
-		it('should mark OAuth1 endpoints with req.proxy', function(done){
+		it('should mark OAuth1 endpoints with req.proxy', function(done) {
 
 			// Override
 			testable.oauth.version = 1;
 
 			hello('testable')
-			.api("/endpoint", function(res){
+			.api('/endpoint', function(res) {
 
 				// Renew
 				testable.oauth.version = 2;
 
 				// Test
-				expect( res.proxy ).to.be.ok();
-				expect( res.oauth_proxy ).to.eql( hello.settings.oauth_proxy );
+				expect(res.proxy).to.be.ok();
+				expect(res.oauth_proxy).to.eql(hello.settings.oauth_proxy);
 
 				done();
 			});
@@ -181,48 +174,49 @@ describe('hello.api', function(){
 
 	});
 
-	describe("map", function(){
+	describe('map', function() {
 
-		it('should process req object through the modules.get[req.path] function', function(done){
+		it('should process req object through the modules.get[req.path] function', function(done) {
 
 			testable.get = testable.get || {};
-			testable.get.handled = function( p ){
-				expect( p ).to.have.property('path','handled');
+			testable.get.handled = function(p) {
+				expect(p).to.have.property('path', 'handled');
 				done();
 			};
 
 			hello('testable')
-			.api("/handled", {a:'a'});
+			.api('/handled', {a: 'a'});
 		});
 
-		it('should process req object through the modules.get.default function if req.path not in module.get', function(done){
+		it('should process req object through the modules.get.default function if req.path not in module.get', function(done) {
 
 			testable.get = testable.get || {};
-			testable.get['default'] = function( p ){
-				expect( p ).to.have.property('path','unhandled');
+			testable.get['default'] = function(p) {
+				expect(p).to.have.property('path', 'unhandled');
 				delete testable.get['default'];
 				done();
 			};
 
 			hello('testable')
-			.api("/unhandled", {a:'a'});
+			.api('/unhandled', {a: 'a'});
 		});
 
-
-		it('should process req object through the modules.get[req.path] map string', function(done){
+		it('should process req object through the modules.get[req.path] map string', function(done) {
 
 			testable.get = testable.get || {};
 			testable.get.handled = 'endpoint?b=@{a}';
 
 			hello('testable')
-			.api("/handled", {a:'a'}, function(res){
+			.api('/handled', {a: 'a'}, function(res) {
 
 				// Should place the value of a in the parameter list
-				expect( res.url ).to.contain('endpoint?b=a');
+				expect(res.url).to.contain('endpoint?b=a');
+
 				// Should place the value of a in the parameter list
-				expect( res.path ).to.eql('handled');
+				expect(res.path).to.eql('handled');
+
 				// Should remove the property from the req.query
-				expect( res.query ).to.not.have.property('a');
+				expect(res.query).to.not.have.property('a');
 
 				delete testable.get.handled;
 
@@ -230,17 +224,17 @@ describe('hello.api', function(){
 			});
 		});
 
-		it('should trigger an error if there was no value for a map string argument', function(done){
+		it('should trigger an error if there was no value for a map string argument', function(done) {
 
 			testable.get = testable.get || {};
 			testable.get.handled = 'endpoint?b=@{a}';
 
 			hello('testable')
-			.api("/handled")
-			.then( null, function(res){
+			.api('/handled')
+			.then(null, function(res) {
 
 				// Should place the value of a in the parameter list
-				expect( res.error ).to.have.property('code', 'missing_attribute');
+				expect(res.error).to.have.property('code', 'missing_attribute');
 
 				delete testable.get.handled;
 
@@ -248,79 +242,77 @@ describe('hello.api', function(){
 			});
 		});
 
-		it('should trigger an error if the mapped value is false', function(done){
+		it('should trigger an error if the mapped value is false', function(done) {
 
 			testable.get = testable.get || {};
 			testable.get.handled = false;
 
 			hello('testable')
-			.api("/handled")
-			.then( null, function(res){
+			.api('/handled')
+			.then(null, function(res) {
 
 				// Should place the value of a in the parameter list
-				expect( res.error ).to.have.property('code', 'invalid_path');
+				expect(res.error).to.have.property('code', 'invalid_path');
 
 				delete testable.get.handled;
 
 				done();
 			});
 		});
-
 	});
-	describe("wrap", function(){
 
-		it('should trigger the wrap function', function(done){
+	describe('wrap', function() {
+
+		it('should trigger the wrap function', function(done) {
 
 			testable.wrap = testable.wrap || {};
-			testable.wrap.handled = function(req){
+			testable.wrap.handled = function(req) {
 				delete testable.wrap.handled;
 				done();
 			};
 
 			hello('testable')
-			.api("/handled");
+			.api('/handled');
 		});
 
-		it('should trigger the wrap.default function if none exists', function(done){
+		it('should trigger the wrap.default function if none exists', function(done) {
 
 			testable.wrap = testable.wrap || {};
-			testable.wrap['default'] = function(req){
+			testable.wrap['default'] = function(req) {
 				delete testable.wrap['default'];
 				done();
 			};
 
 			hello('testable')
-			.api("/unhandled");
+			.api('/unhandled');
 		});
 	});
 
+	describe('paging', function() {
 
-	describe('paging', function(){
-
-
-		it('should override the path parameter with the hash fragment', function(done){
+		it('should override the path parameter with the hash fragment', function(done) {
 
 			hello('testable')
-			.api("/endpoint#formatting", function(res){
-				expect( res.url ).to.eql( 'https://testable/endpoint' );
-				expect( res.path ).to.eql( 'formatting' );
+			.api('/endpoint#formatting', function(res) {
+				expect(res.url).to.eql('https://testable/endpoint');
+				expect(res.path).to.eql('formatting');
 				done();
 			});
 		});
 
-		it('should append the req.path to the hash of the response.paging.next', function(done){
+		it('should append the req.path to the hash of the response.paging.next', function(done) {
 
 			testable.wrap = testable.wrap || {};
-			testable.wrap['default'] = function(req){
+			testable.wrap['default'] = function(req) {
 				req.paging = {next:'next?page=2'};
 				delete testable.wrap['default'];
 			};
 
 			hello('testable')
-			.api("/unhandled", function(res){
+			.api('/unhandled', function(res) {
 
 				// Should place the value of a in the parameter list
-				expect( res.paging.next ).to.contain('#unhandled');
+				expect(res.paging.next).to.contain('#unhandled');
 
 				done();
 
@@ -328,7 +320,6 @@ describe('hello.api', function(){
 		});
 
 	});
-
 
 });
 
