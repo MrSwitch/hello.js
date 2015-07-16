8693c93383c96d8fa9f15361aac3e34e0fc1f035
Yury Oparin <yury.oparin@gmail.com>
2015-07-17 01:21:48 +0300

    Added vk.js

--- src/modules/vk.js
+++ src/modules/vk.js
@@ -1,71 +1,77 @@
+// Vkontakte (vk.com)
 (function(hello) {
-    hello.init({
-        vk: {
-            name: 'Vk',
+	
+	hello.init({
+		
+		vk: {
+			name: 'Vk',
 
-            // See https://vk.com/dev/oauth_dialog
-            oauth: {
-                version: 2,
-                auth: 'https://oauth.vk.com/authorize',
-                grant: 'https://oauth.vk.com/access_token'
-            },
+			// See https://vk.com/dev/oauth_dialog
+			oauth: {
+				version: 2,
+				auth: 'https://oauth.vk.com/authorize',
+				grant: 'https://oauth.vk.com/access_token'
+			},
+			
+			// Authorization scopes
+			scope: {
+				basic: '',
+				email: 'email',
+				offline_access: 'offline'
+			},
 
-            // Authorization scopes
-            scope: {
-                basic: '',
-                email: 'email',
-                offline_access: 'offline'
-            },
+			// Refresh the access_token
+			refresh: true,
 
-            // Refresh the access_token
-            refresh: true,
+			login: function(p) {
+				p.qs.display = window.navigator && 
+					window.navigator.userAgent && 
+					/ipad|phone|phone|android/.test(window.navigator.userAgent.toLowerCase()) 
+					? 'mobile' : 'popup';
+			},
 
-            login: function(p) {
-                p.qs.display = window.navigator && window.navigator.userAgent && /ipad|phone|phone|android/.test(window.navigator.userAgent.toLowerCase()) ? 'mobile' : 'popup';
-            },
+			// API Base URL
+			base: 'https://api.vk.com/method/',
 
-            // API Base URL
-            base: 'https://api.vk.com/method/',
+			// Map GET requests
+			get: {
+				me: 'users.get'
+			},
 
-            // Map GET requests
-            get: {
-                me: 'users.get'
-            },
+			// Map POST requests
+			post: false,
 
-            // Map POST requests
-            post: false,
+			wrap: {
+				me: formatUser
+			},
 
-            wrap: {
-                me: formatUser
-            },
+			// No XHR
+			xhr: false,
 
-            // No XHR
-            xhr: false,
+			// All requests should be JSONP as of missing CORS headers in https://api.vk.com/method/*
+			jsonp: function(p, qs) {
+				if (p.path === 'me') {
+					qs.fields = 'id,first_name,last_name,photo_max';
+				}
+				return true;
+			},
 
-            // All requests should be JSONP as of missing CORS headers in https://api.vk.com/method/*
-            jsonp: function(p, qs) {
-                if (p.path === 'me') {
-                    qs.fields = 'id,first_name,last_name,photo_max';
-                }
-                return true;
-            },
+			// No form
+			form: false
+		}
+	});
 
-            // No form
-            form: false
-        }
-    });
+	function formatUser(o) {
+		if (o != null && o.response != null && o.response.length) {
+			o = o.response[0];
+			o.id = o.uid;
+			o.thumbnail = o.picture = o.photo_max;
+			o.name = o.first_name + ' ' + o.last_name;
 
-    function formatUser(o) {
-        if (o != null && o.response != null && o.response.length) {
-            o = o.response[0];
-            o.id = o.uid;
-            o.thumbnail = o.picture = o.photo_max;
-            o.name = o.first_name + ' ' + o.last_name;
-
-            var vk = hello.utils.store('vk');
-            if (vk != null && vk.email != null)
-                o.email = vk.email;
-        }
-        return o;
-    }
+			var vk = hello.utils.store('vk');
+			if (vk != null && vk.email != null)
+				o.email = vk.email;
+		}
+		return o;
+	}
 })(hello);
