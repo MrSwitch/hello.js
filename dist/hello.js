5568744fa5b0ee4b5920c93ce3c56c5583e4cffa
Jordan Thoms <jordan@thoms.net.nz>
2016-01-07 13:28:57 +1300

    Update list

--- dist/hello.js
+++ dist/hello.js
@@ -1,4 +1,4 @@
-/*! hellojs v1.9.8 | (c) 2012-2015 Andrew Dodson | MIT https://adodson.com/hello.js/LICENSE */
+/*! hellojs v1.9.8 | (c) 2012-2016 Andrew Dodson | MIT https://adodson.com/hello.js/LICENSE */
 // ES5 Object.create
 if (!Object.create) {
 
@@ -515,6 +515,12 @@ hello.utils.extend(hello, {
 			// Add the proxy url
 			p.qs.state.oauth_proxy = opts.oauth_proxy;
 
+		}
+
+		// If we are running as a chrome ext, add the extension ID to the state so the popup can
+		// post a message back to us
+		if (typeof chrome === 'object' && typeof chrome.extension === 'object') {
+			p.qs.state.chrome_ext_id = opts.chrome_ext_id;
 		}
 
 		// Convert state to a string
@@ -1653,6 +1659,19 @@ hello.utils.extend(hello.utils, {
 				catch (e) {
 					// Error thrown whilst executing parent callback
 				}
+			}
+			else if (obj.chrome_ext_id && typeof chrome === 'object' && typeof chrome.runtime === 'object') {
+				// If we have a chrome extension ID, try using the chrome APIs
+				try {
+					delete obj.callback;
+				}
+				catch (e) {}
+
+				message = {};
+				message.message_type = 'google_auth_response';
+				message.callback = cb;
+				message.obj = JSON.stringify(obj);
+				chrome.runtime.sendMessage(obj.chrome_ext_id, message);
 			}
 
 			closeWindow();
@@ -2873,130 +2892,14 @@ hello.utils.extend(hello.utils, {
 })(hello);
 
 // Script to support ChromeApps
-// This overides the hello.utils.popup method to support chrome.identity.launchWebAuthFlow
-// See https://developer.chrome.com/apps/app_identity#non
-
-// Is this a chrome app?
-
-if (typeof chrome === 'object' && typeof chrome.identity === 'object' && chrome.identity.launchWebAuthFlow) {
-
-	(function() {
-
-		// Swap the popup method
-		hello.utils.popup = function(url) {
-
-			return _open(url, true);
-
-		};
-
-		// Swap the hidden iframe method
-		hello.utils.iframe = function(url) {
-
-			_open(url, false);
-
-		};
-
-		// Swap the request_cors method
-		hello.utils.request_cors = function(callback) {
-
-			callback();
-
-			// Always run as CORS
-
-			return true;
-		};
-
-		// Swap the storage method
-		var _cache = {};
-		chrome.storage.local.get('hello', function(r) {
-			// Update the cache
-			_cache = r.hello || {};
-		});
-
-		hello.utils.store = function(name, value) {
-
-			// Get all
-			if (arguments.length === 0) {
-				return _cache;
-			}
-
-			// Get
-			if (arguments.length === 1) {
-				return _cache[name] || null;
-			}
-
-			// Set
-			if (value) {
-				_cache[name] = value;
-				chrome.storage.local.set({hello: _cache});
-				return value;
-			}
-
-			// Delete
-			if (value === null) {
-				delete _cache[name];
-				chrome.storage.local.set({hello: _cache});
-				return null;
-			}
-		};
-
-		// Open function
-		function _open(url, interactive) {
-
-			// Launch
-			var ref = {
-				closed: false
-			};
-
-			// Launch the webAuthFlow
-			chrome.identity.launchWebAuthFlow({
-				url: url,
-				interactive: interactive
-			}, function(responseUrl) {
-
-				// Did the user cancel this prematurely
-				if (responseUrl === undefined) {
-					ref.closed = true;
-					return;
-				}
-
-				// Split appart the URL
-				var a = hello.utils.url(responseUrl);
-
-				// The location can be augmented in to a location object like so...
-				// We dont have window operations on the popup so lets create some
-				var _popup = {
-					location: {
-
-						// Change the location of the popup
-						assign: function(url) {
-
-							// If there is a secondary reassign
-							// In the case of OAuth1
-							// Trigger this in non-interactive mode.
-							_open(url, false);
-						},
-
-						search: a.search,
-						hash: a.hash,
-						href: a.href
-					},
-					close: function() {}
-				};
-
-				// Then this URL contains information which HelloJS must process
-				// URL string
-				// Window - any action such as window relocation goes here
-				// Opener - the parent window which opened this, aka this script
-
-				hello.utils.responseHandler(_popup, window);
-			});
-
-			// Return the reference
-			return ref;
+// This registers a listener for the message from the popup window
+// Is this a chrome extension?
+if (typeof chrome === 'object' && typeof chrome.extension === 'object') {
+	chrome.runtime.onMessageExternal.addListener(function(data) {
+		if (data.message_type == 'google_auth_response') {
+			window[data.callback](data.obj);
 		}
-
-	})();
+	});
 }
 
 // Phonegap override for hello.phonegap.js
