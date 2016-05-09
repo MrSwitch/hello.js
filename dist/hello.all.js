4b742d30b508d28c1fbef55332cca7f6c8995284
Andrew Dodson <andrew@5app.com>
2016-05-09 12:39:28 +0100

    Fix jshint version with node@6

--- dist/hello.all.js
+++ dist/hello.all.js
@@ -1,4 +1,4 @@
-/*! hellojs v1.13.0 | (c) 2012-2016 Andrew Dodson | MIT https://adodson.com/hello.js/LICENSE */
+/*! hellojs v1.13.1 | (c) 2012-2016 Andrew Dodson | MIT https://adodson.com/hello.js/LICENSE */
 // ES5 Object.create
 if (!Object.create) {
 
