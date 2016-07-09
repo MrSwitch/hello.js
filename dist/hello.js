0a5b1f66694c03efd998a251ff5f33a5b7ffd89c
Andrew Dodson <andrew@5app.com>
2016-07-09 09:21:49 +0100

    Fix release 1.13.2 non-updated dist

--- dist/hello.js
+++ dist/hello.js
@@ -1,4 +1,4 @@
-/*! hellojs v1.13.2 | (c) 2012-2016 Andrew Dodson | MIT https://adodson.com/hello.js/LICENSE */
+/*! hellojs v1.13.3 | (c) 2012-2016 Andrew Dodson | MIT https://adodson.com/hello.js/LICENSE */
 // ES5 Object.create
 if (!Object.create) {
 
