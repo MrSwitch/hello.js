51134eccf86f306f9f17e8071dfc0c40ac230d7f
berkeleycole <ahope@ahopemindgruvenet.local>
2019-03-15 10:39:17 -0700

    wraps all files that rely on browser window or document in a conditional

--- src/hello.chromeapp.js
+++ src/hello.chromeapp.js
@@ -3,124 +3,125 @@
 // See https://developer.chrome.com/apps/app_identity#non
 
 // Is this a chrome app?
+if (typeof window !== 'undefined') {
+	if (typeof chrome === 'object' && typeof chrome.identity === 'object' && chrome.identity.launchWebAuthFlow) {
 
-if (typeof chrome === 'object' && typeof chrome.identity === 'object' && chrome.identity.launchWebAuthFlow) {
+		(function() {
 
-	(function() {
+			// Swap the popup method
+			hello.utils.popup = function(url) {
 
-		// Swap the popup method
-		hello.utils.popup = function(url) {
+				return _open(url, true);
 
-			return _open(url, true);
-
-		};
-
-		// Swap the hidden iframe method
-		hello.utils.iframe = function(url) {
-
-			_open(url, false);
-
-		};
-
-		// Swap the request_cors method
-		hello.utils.request_cors = function(callback) {
-
-			callback();
-
-			// Always run as CORS
-
-			return true;
-		};
-
-		// Swap the storage method
-		var _cache = {};
-		chrome.storage.local.get('hello', function(r) {
-			// Update the cache
-			_cache = r.hello || {};
-		});
-
-		hello.utils.store = function(name, value) {
-
-			// Get all
-			if (arguments.length === 0) {
-				return _cache;
-			}
-
-			// Get
-			if (arguments.length === 1) {
-				return _cache[name] || null;
-			}
-
-			// Set
-			if (value) {
-				_cache[name] = value;
-				chrome.storage.local.set({hello: _cache});
-				return value;
-			}
-
-			// Delete
-			if (value === null) {
-				delete _cache[name];
-				chrome.storage.local.set({hello: _cache});
-				return null;
-			}
-		};
-
-		// Open function
-		function _open(url, interactive) {
-
-			// Launch
-			var ref = {
-				closed: false
 			};
 
-			// Launch the webAuthFlow
-			chrome.identity.launchWebAuthFlow({
-				url: url,
-				interactive: interactive
-			}, function(responseUrl) {
+			// Swap the hidden iframe method
+			hello.utils.iframe = function(url) {
 
-				// Did the user cancel this prematurely
-				if (responseUrl === undefined) {
-					ref.closed = true;
-					return;
-				}
+				_open(url, false);
 
-				// Split appart the URL
-				var a = hello.utils.url(responseUrl);
+			};
 
-				// The location can be augmented in to a location object like so...
-				// We dont have window operations on the popup so lets create some
-				var _popup = {
-					location: {
+			// Swap the request_cors method
+			hello.utils.request_cors = function(callback) {
 
-						// Change the location of the popup
-						assign: function(url) {
+				callback();
 
-							// If there is a secondary reassign
-							// In the case of OAuth1
-							// Trigger this in non-interactive mode.
-							_open(url, false);
-						},
+				// Always run as CORS
 
-						search: a.search,
-						hash: a.hash,
-						href: a.href
-					},
-					close: function() {}
-				};
+				return true;
+			};
 
-				// Then this URL contains information which HelloJS must process
-				// URL string
-				// Window - any action such as window relocation goes here
-				// Opener - the parent window which opened this, aka this script
-
-				hello.utils.responseHandler(_popup, window);
+			// Swap the storage method
+			var _cache = {};
+			chrome.storage.local.get('hello', function(r) {
+				// Update the cache
+				_cache = r.hello || {};
 			});
 
-			// Return the reference
-			return ref;
-		}
+			hello.utils.store = function(name, value) {
 
-	})();
+				// Get all
+				if (arguments.length === 0) {
+					return _cache;
+				}
+
+				// Get
+				if (arguments.length === 1) {
+					return _cache[name] || null;
+				}
+
+				// Set
+				if (value) {
+					_cache[name] = value;
+					chrome.storage.local.set({hello: _cache});
+					return value;
+				}
+
+				// Delete
+				if (value === null) {
+					delete _cache[name];
+					chrome.storage.local.set({hello: _cache});
+					return null;
+				}
+			};
+
+			// Open function
+			function _open(url, interactive) {
+
+				// Launch
+				var ref = {
+					closed: false
+				};
+
+				// Launch the webAuthFlow
+				chrome.identity.launchWebAuthFlow({
+					url: url,
+					interactive: interactive
+				}, function(responseUrl) {
+
+					// Did the user cancel this prematurely
+					if (responseUrl === undefined) {
+						ref.closed = true;
+						return;
+					}
+
+					// Split appart the URL
+					var a = hello.utils.url(responseUrl);
+
+					// The location can be augmented in to a location object like so...
+					// We dont have window operations on the popup so lets create some
+					var _popup = {
+						location: {
+
+							// Change the location of the popup
+							assign: function(url) {
+
+								// If there is a secondary reassign
+								// In the case of OAuth1
+								// Trigger this in non-interactive mode.
+								_open(url, false);
+							},
+
+							search: a.search,
+							hash: a.hash,
+							href: a.href
+						},
+						close: function() {}
+					};
+
+					// Then this URL contains information which HelloJS must process
+					// URL string
+					// Window - any action such as window relocation goes here
+					// Opener - the parent window which opened this, aka this script
+
+					hello.utils.responseHandler(_popup, window);
+				});
+
+				// Return the reference
+				return ref;
+			}
+
+		})();
+	}
 }
--- src/modules/vk.js
+++ src/modules/vk.js
@@ -1,91 +1,93 @@
 // Vkontakte (vk.com)
-(function(hello) {
+if (typeof window !== 'undefined') {
+	(function(hello) {
 
-	hello.init({
+		hello.init({
 
-		vk: {
-			name: 'Vk',
+			vk: {
+				name: 'Vk',
 
-			// See https://vk.com/dev/oauth_dialog
-			oauth: {
-				version: 2,
-				auth: 'https://oauth.vk.com/authorize',
-				grant: 'https://oauth.vk.com/access_token'
-			},
+				// See https://vk.com/dev/oauth_dialog
+				oauth: {
+					version: 2,
+					auth: 'https://oauth.vk.com/authorize',
+					grant: 'https://oauth.vk.com/access_token'
+				},
 
-			// Authorization scopes
-			// See https://vk.com/dev/permissions
-			scope: {
-				email: 'email',
-				friends: 'friends',
-				photos: 'photos',
-				videos: 'video',
-				share: 'share',
-				offline_access: 'offline'
-			},
+				// Authorization scopes
+				// See https://vk.com/dev/permissions
+				scope: {
+					email: 'email',
+					friends: 'friends',
+					photos: 'photos',
+					videos: 'video',
+					share: 'share',
+					offline_access: 'offline'
+				},
 
-			// Refresh the access_token
-			refresh: true,
+				// Refresh the access_token
+				refresh: true,
 
-			login: function(p) {
-				p.qs.display = window.navigator &&
-					window.navigator.userAgent &&
-					/ipad|phone|phone|android/.test(window.navigator.userAgent.toLowerCase()) ? 'mobile' : 'popup';
-			},
+				login: function(p) {
+					p.qs.display = window.navigator &&
+						window.navigator.userAgent &&
+						/ipad|phone|phone|android/.test(window.navigator.userAgent.toLowerCase()) ? 'mobile' : 'popup';
+				},
 
-			// API Base URL
-			base: 'https://api.vk.com/method/',
+				// API Base URL
+				base: 'https://api.vk.com/method/',
 
-			// Map GET requests
-			get: {
-				me: function(p, callback) {
-					p.query.fields = 'id,first_name,last_name,photo_max';
-					callback('users.get');
-				}
-			},
+				// Map GET requests
+				get: {
+					me: function(p, callback) {
+						p.query.fields = 'id,first_name,last_name,photo_max';
+						callback('users.get');
+					}
+				},
 
-			wrap: {
-				me: function(res, headers, req) {
-					formatError(res);
-					return formatUser(res, req);
-				}
-			},
+				wrap: {
+					me: function(res, headers, req) {
+						formatError(res);
+						return formatUser(res, req);
+					}
+				},
 
-			// No XHR
-			xhr: false,
+				// No XHR
+				xhr: false,
 
-			// All requests should be JSONP as of missing CORS headers in https://api.vk.com/method/*
-			jsonp: true,
+				// All requests should be JSONP as of missing CORS headers in https://api.vk.com/method/*
+				jsonp: true,
 
-			// No form
-			form: false
+				// No form
+				form: false
+			}
+		});
+
+		function formatUser(o, req) {
+
+			if (o !== null && 'response' in o && o.response !== null && o.response.length) {
+				o = o.response[0];
+				o.id = o.uid;
+				o.thumbnail = o.picture = o.photo_max;
+				o.name = o.first_name + ' ' + o.last_name;
+
+				if (req.authResponse && req.authResponse.email !== null)
+					o.email = req.authResponse.email;
+			}
+
+			return o;
 		}
-	});
 
-	function formatUser(o, req) {
+		function formatError(o) {
 
-		if (o !== null && 'response' in o && o.response !== null && o.response.length) {
-			o = o.response[0];
-			o.id = o.uid;
-			o.thumbnail = o.picture = o.photo_max;
-			o.name = o.first_name + ' ' + o.last_name;
-
-			if (req.authResponse && req.authResponse.email !== null)
-				o.email = req.authResponse.email;
+			if (o.error) {
+				var e = o.error;
+				o.error = {
+					code: e.error_code,
+					message: e.error_msg
+				};
+			}
 		}
 
-		return o;
-	}
-
-	function formatError(o) {
-
-		if (o.error) {
-			var e = o.error;
-			o.error = {
-				code: e.error_code,
-				message: e.error_msg
-			};
-		}
-	}
-
-})(hello);
+	})(hello);
+}
